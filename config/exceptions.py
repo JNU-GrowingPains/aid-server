0c3f7f4c5aaa9a8bff3155868b68388bde8f432d
seorin-yu <seorin.yu@navercorp.com>
2025-12-20 21:02:21 +0900

    fix: exception fast api

--- config/exceptions.py
+++ config/exceptions.py
@@ -1,9 +1,10 @@
 # app/core/exceptions.py
-from fastapi import Request, status
+from fastapi import Request, status, HTTPException
 from fastapi.responses import JSONResponse
 
 async def value_error_handler(request: Request, exc: ValueError):
+    raise HTTPException(status_code=404, detail="User not found")
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content={"detail": str(exc)},
-    )
+    )ㅗ토
